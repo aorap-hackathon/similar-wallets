138ca7766898b55739ac5a4d16171934de428366
aorap-hackathon <a.orap@icloud.com>
2024-09-29 21:54:48 +0200

    adding some api keys

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
+    SCROLLSCAN_API_KEY: process.env.SCROLLSCAN_API_KEY,
+  }
 };
 
 module.exports = nextConfig;
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,17 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { http } from 'wagmi';
 import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
+  scroll
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: 'Similar Wallets',
+  projectId: 'a0423bd4d5dfb377c736fc030a4b2f93',
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+    scroll
   ],
+  transports: {
+    [scroll.id]: http(`https://scroll-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`),
+  },
   ssr: true,
 });
